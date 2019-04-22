a5bfd1b275d4a72327a33b1e9cfb9f5510af8d9a
fjsanti <black02a98@gmail.com>
2019-04-22 21:19:12 +0000

    sdm660-common: add carrier apps

--- Android.mk
+++ Android.mk
@@ -90,6 +90,16 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
+DMSERVICE_LIBS := libdmengine.so libdmjavaplugin.so
+DMSERVICE_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/DMService/lib/arm/,$(notdir $(DMSERVICE_LIBS)))
+$(DMSERVICE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "DMService lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib/$(notdir $@) $@
+
+ ALL_DEFAULT_INSTALLED_MODULES += $(DMSERVICE_SYMLINKS)
+
 RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT_VENDOR)/rfs/msm/adsp/
 $(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating RFS MSM ADSP folder structure: $@"
